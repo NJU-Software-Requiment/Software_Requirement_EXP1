83501ad7890aa7082aec696e159f54a6ac793339
Ricardokevins <3121416933@qq.com>
2020-11-01 21:32:26 +0800

    word2vec All model update

--- word2vec.py
+++ word2vec.py
@@ -5,22 +5,28 @@ import multiprocessing
 
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
-file_name='vscode_issues_title'
-inp='data/'+file_name+'.txt'
-outp1='Model/'+file_name+'.model'
-def train():
+
+file_names=['ide_ques','vscode_issues_title','vscode_pr_title']
+def train(inp,outp):
     #outp2 = 'Model/test.vector'
     model = Word2Vec(LineSentence(inp), size=200, window=5, min_count=5,
                      workers=multiprocessing.cpu_count())
-    model.save(outp1)
+    model.save(outp)
     #model.wv.save_word2vec_format(outp2, binary=False)
 
+retrain=True
+if retrain==True:
+    for i in file_names:
+        inp = 'data/' + i + '.txt'
+        outp1 = 'Model/' + i + '.model'
+        print("training: ",outp1)
+        train(inp,outp1)
 
-train()
-model = Word2Vec.load(outp1)
+def test(model):
+    model = Word2Vec.load(model)
 
-testwords = ['debug', 'editor', 'console', 'code']
-for i in range(len(testwords)):
-    res = model.most_similar(testwords[i],topn=10)
-    print(testwords[i])
-    print(res)
+    testwords = ['debug', 'editor', 'console', 'code']
+    for i in range(len(testwords)):
+        res = model.most_similar(testwords[i],topn=10)
+        print(testwords[i])
+        print(res)
